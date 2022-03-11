27b4ab136d8411db1cae76f44561ab6fad5a7466
Favour-dgreat <53022031+Favour-dgreat@users.noreply.github.com>
2022-03-11 01:40:29 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ const getServiceHires = async function (serviceIndex, hiresLength) {
 
 const getServices = async function() {
   const _servicesLength = await contract.methods.getServicesLength().call()
-  document.querySelector("#trainersCounts").textContent = _servicesLength + ' services';
+  document.querySelector("#trainersCounts").textContent = _servicesLength + ' agents';
 
   const _services = []
     for (let i = 0; i < _servicesLength; i++) {
